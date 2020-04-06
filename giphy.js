3441ee20cdd03d052efb75a4055ae35c1e608a96
kidmoney64 <kidmoney64@users.noreply.github.com>
2020-04-06 15:48:12 -0500

    installed the java

---
+++ giphy.js
@@ -0,0 +1,18 @@
+
+function $getdata() {
+  
+    var input = $("#searchtext").val()
+
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=mHWTM2zQ75bsA37jtTPoHVrd3Bf0JXdy&limit=5");
+xhr.done(function(response) { 
+    console.log("success got data", response);
+    
+    var jiffs = response.data
+    
+    for (i in jiffs)
+    {
+    $('.inner').append("<img src='"+jiffs[i].images.original.url+"' styles='height:350px; width:350px;'/>")
+    }
+ });
+
+}
