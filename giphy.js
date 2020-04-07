ea54b5c4db333fab27db8a102ebc12f354cae7b1
kidmoney64 <kidmoney64@users.noreply.github.com>
2020-04-07 18:04:00 -0500

    refixed the javascript

--- giphy.js
+++ giphy.js
@@ -1,17 +1,22 @@
-function $getdata() {
-  
-    var input = $("#searchtext").val()
+ 
 
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=+input+&api_key=mHWTM2zQ75bsA37jtTPoHVrd3Bf0JXdy&limit=5");
-xhr.done(function(response) { 
-    console.log("success got data", response);
-    
-    var jiffs = response.data
-    
-    for (i in jiffs)
-    {
-    $('.inner').append("<img src='"+jiffs[i].images.original.url+"' styles='height:350px; width:350px;'/>")
-    }
- });
+ 
+function getdata() {
+
+    var inputValue = $("#searchtext").val();
+
+    var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${ inputValue }&api_key=mHWTM2zQ75bsA37jtTPoHVrd3Bf0JXdy&limit=5`);
+    xhr.done(function(response) { 
+        console.log("success got data", response);
+        
+        var jiffs = response.data
+        
+        for (i in jiffs)
+        {
+        $('.inner').append("<img src='"+jiffs[i].images.original.url+"' styles='height:350px; width:350px;'/>")
+        }
+    });
 
 }
+
+
