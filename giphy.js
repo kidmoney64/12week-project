f45fe3d7a9d1e2f6ae2637478269b3292b0baac6
kidmoney64 <kidmoney64@users.noreply.github.com>
2020-04-14 11:49:46 -0500

    added the enter button on click

--- giphy.js
+++ giphy.js
@@ -3,16 +3,22 @@ function getdata() {
 
     var inputValue = $("#searchtext").val();
 
+    
     var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${ inputValue }&api_key=mHWTM2zQ75bsA37jtTPoHVrd3Bf0JXdy&limit=5`);
     xhr.done(function(response) { 
         console.log("success got data", response);
-        
         var jiffs = response.data
-        
-        for (i in jiffs)
-        {
-        $('.inner').append("<img src='"+jiffs[i].images.original.url+"' styles='height:350px; width:350px;'/>")
-        }
+        $('.jiffs').empty();
+        for (i in jiffs){
+        $('.jiffs').append("<img src='"+jiffs[i].images.original.url+"' styles='height:350px; width:350px;'/>")
+        };
+    var input = document.getElementById("searchtext");
+        input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("searchgifs").click();
+  }
+});
     });
 
 }
