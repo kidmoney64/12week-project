c825a1b03eaff1adad53046f687ec35216a1c161
kidmoney64 <kidmoney64@users.noreply.github.com>
2020-04-14 14:20:17 -0500

    update css

--- giphy.js
+++ giphy.js
@@ -3,13 +3,13 @@ function getdata() {
 
     var inputValue = $("#searchtext").val();
 
-    var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${ inputValue }&api_key=mHWTM2zQ75bsA37jtTPoHVrd3Bf0JXdy&limit=30`);
+    var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${ inputValue }&api_key=mHWTM2zQ75bsA37jtTPoHVrd3Bf0JXdy&limit=20`);
     xhr.done(function(response) { 
         console.log("success got data", response);
         var jiffs = response.data
         $('.jiffs').empty();
         for (i in jiffs){
-        $('.jiffs').append("<img src='"+jiffs[i].images.original.url+"' styles='height:100px; width:100px;'/>")
+        $('.jiffs').append("<img src='"+jiffs[i].images.original.url+"' styles='height:100px; width:100px; padding: 50px;'/>")
         };
     var input = document.getElementById("searchtext");
         input.addEventListener("keyup", function(event) {
