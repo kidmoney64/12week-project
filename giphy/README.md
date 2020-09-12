d5c08827940c3ce373a2d4fd4954b7449e6f3f07
kidmoney64 <kidmoney64@users.noreply.github.com>
2020-09-11 20:46:07 -0500

    add folders

--- giphy/README.md
+++ giphy/README.md
@@ -41,3 +41,39 @@ giphy
       <script src="js/giphy.js"></script>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+$('.jiffs').append("<img src='"+jiffs[i].images.original.url+"' styles='height:100px; width:100px; padding: 50px;'/>")
+
+
+
+ 
+function getdata() {
+
+    var inputValue = $("#searchtext").val();
+
+    var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${ inputValue }&api_key=T4Qmn41LExNQ1yz9ItjjQtB8RZ64AyVp&limit=20`);
+    xhr.done(function(response) { 
+        console.log("success got data", response);
+        var jiffs = response.data
+        $('.jiffs').empty();
+        for (i in jiffs){
+        $('.jiffs').append("<img src='"+jiffs[i].images.original.url+"' styles='height:100px; width:100px; padding: 100px;'/>")
+        };
+    var input = document.getElementById("searchtext");
+        input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("searchgifs").click();
+  }
+});
+    });
+
+}
---
+++ giphy/html/js/giphy.js
@@ -0,0 +1,45 @@
+/*function getdata() {
+
+    var inputValue = $("#searchtext").val();
+
+    var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${ inputValue }&api_key=T4Qmn41LExNQ1yz9ItjjQtB8RZ64AyVp&limit=20`);
+    xhr.done(function(response) { 
+        console.log("success got data", response);
+        var jiffs = response.data
+        $('.jiffs').empty();
+        for (i in jiffs){
+        $('.jiffs').append("<img src='jiffs[i].images.original.url'/>")
+        };
+    var input = document.getElementById("searchtext");
+        input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("searchgifs").click();
+  }
+});
+    });
+
+}
+*/
+function getdata() {
+
+    var inputValue = $("#searchtext").val();
+
+    var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${ inputValue }&api_key=T4Qmn41LExNQ1yz9ItjjQtB8RZ64AyVp&limit=20`);
+    xhr.done(function(response) { 
+        console.log("success got data", response);
+        var jiffs = response.data
+        $('.jiffs').empty();
+        for (i in jiffs){
+        $('.jiffs').append("<img src='"+jiffs[i].images.original.url+"' styles='height:100px; width:100px; padding: 100px;'/>")
+        };
+    var input = document.getElementById("searchtext");
+        input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("searchgifs").click();
+  }
+});
+    });
+
+}
