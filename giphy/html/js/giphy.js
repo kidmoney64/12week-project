3c7641f3ed30944aabbd16a8e83220bc97cd1da0
kidmoney64 <kidmoney64@users.noreply.github.com>
2020-09-29 19:33:31 -0500

    add a dino img

--- giphy/html/js/giphy.js
+++ giphy/html/js/giphy.js
@@ -1,22 +1,25 @@
 function getdata() {
 
-  var inputValue = $("#searchtext").val();
 
-  var xhr = $.get(`https://api.giphy.com/v1/gifs/search?q=${ inputValue }&api_key=T4Qmn41LExNQ1yz9ItjjQtB8RZ64AyVp&limit=20`);
-  xhr.done(function(response) { 
+    var inputValue = $("#searchtext").val();
+
+    var xhr = $.get(`https://api.giphy.com/v1/gifs/search?q=${ inputValue }&api_key=T4Qmn41LExNQ1yz9ItjjQtB8RZ64AyVp&limit=20`);
+    xhr.done(function(response) { 
       console.log("success got data", response);
       var jiffs = response.data
       $('.jiffs').empty();
       for (let jiff of jiffs){
-      $('.jiffs').append(`<img src="${jiff.images.original.url}" alt="giphy" class="giphy-image" />`)
+        $('.jiffs').append(`<img src="${jiff.images.original.url}" alt="giphy" class="giphy-image" />`)
       };
+
+    });
+
+  }
+
   var input = document.getElementById("searchtext");
-      input.addEventListener("keyup", function(event) {
-      if (event.keyCode === 13) {
+  input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
       event.preventDefault();
       document.getElementById("searchgifs").click();
-}
-});
-  });
-
-}
+    }
+  });  
